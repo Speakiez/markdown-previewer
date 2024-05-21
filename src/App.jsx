9e5375d3cfb999a3e16816c24defd2fb87962d0e
Speakiez <aerholgocalin57@gmail.com>
2024-05-21 13:57:05 +0800

    Add proper markdown parsing and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
 import { useEffect } from "react"
 import { marked } from "marked"
 
+marked.use({
+  breaks: true
+});
+
 export default function App() {
+  const [userInput, setUserInput] = useState("# Markup Previewer");
+
+  const handleChange = (event) => setUserInput(event.target.value);
 
   useEffect(() => {
-    document.getElementById("preview").innerHTML = marked.parse("# Markup Previewer");
+    document.getElementById("preview").innerHTML = marked.parse(userInput);
   });
 
   return (
     <>
-      <textarea id="editor"></textarea>
+      <textarea id="editor" onChange={handleChange}>{userInput}</textarea>
       <div id="preview"></div>
     </>
   )
